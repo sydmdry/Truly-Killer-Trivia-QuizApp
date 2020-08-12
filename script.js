eb30595349dfbdc46b8c6d3411f31eb50d7c086a
Sydney Dryden <sydneymarierose@gmail.com>
2020-08-12 01:45:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+let data = STORE;
+let questionCount = 0;
+let score = 0;
+
+function beginQuiz(){
+}
+
+function renderQuestion(){
+}
+
+function updateScoreQuestion(){
+}
+
+function correctAnswer(){
+}
+
+function wrongAnswer(){
+}
+
+function restartQuiz(){
+}
+
---
+++ store.js
@@ -0,0 +1,74 @@
+const STORE = [
+ questions: [
+  {
+    question: 'Which murderer was famously known as the "Killer Clown"?',
+    answer: [
+      'Dennis Rader',
+      'John Wayne Gacy',
+      'Krusty the Clown',
+      'Ed Gein'
+    ],
+    correct: 'John Wayne Gacy'
+  },
+  {
+    question: 'What notorious female serial killer posed as a hitch-hiking sex worker to kill seven motorists?',
+    answer: [
+      'Nannie Doss',
+      'Jenna Krasowski',
+      'Genene Jones',
+      'Aileen Wuornos'
+    ],
+    correct: 'Aileen Wuornos'
+  },
+  {
+    question: 'Which cannibal became known for attempting to turn their victims into zombies?',
+    answer: [
+      'Jeffrey Dahmer',
+      'Hannibal Buress',
+      'Luka Magnotta',
+      'Albert Fish'
+    ],
+    correct: 'Jeffrey Dahmer'
+  },
+  {
+    question: 'Who was the child beauty queen who became victim to a still unsolved murder at the age of six in her families home?',
+    answer: [
+      'Tana Ramsay',
+      'Patsy Ramsey',
+      'JonBenét Ramsey',
+      'Gordon Ramsay'
+    ],
+    correct: 'JonBenét Ramsey'
+  },
+  {
+    question: 'What name was given to kidnapper and suspected serial killer David Parker Ray; who is believed to have tortured and killed more than 50 women in his soundproof trailer?',
+    answer: [
+      'Mobile Home Murderer',
+      'Toy Box Killer',
+      'Bunker Butcher',
+      'Trailer Torturer'
+    ],
+    correct: 'Toy Box Killer'
+  },
+  {
+    question: 'Who was the first serial killer to be caught using familial DNA after escaping capture for over 30 years?',
+    answer: [
+      'The Boston Strangler',
+      'The Green River Killer',
+      'The Zodiac Killer',
+      'The Golden State Killer'
+    ],
+    correct: 'The Golden State Killer'
+  },
+  {
+    question: 'What did sixteen year old Brenda Ann Spencer respond when asked why she opened fire on an elementary school across from her home?',
+    answer: [
+      'Why not?',
+      'I wanted to know what it felt like.',
+      "I don't like Mondays.",
+      'I wanted to.'
+    ],
+    correct: "I don't like Mondays."
+  }
+ ]
+];
