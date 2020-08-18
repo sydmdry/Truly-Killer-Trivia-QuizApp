a56ae6ced714a141876f80852c90ec10eacbde95
Sydney Dryden <sydneymarierose@gmail.com>
2020-08-18 00:40:45 +0000

    before moving around divs

--- script.js
+++ script.js
@@ -2,37 +2,150 @@
 let data = STORE;
 let questionCount = 0;
 let score = 0;
+let currentQuestion = 0;
 
 
 //User starts the quiz
 function beginQuiz(){
   $('.createForm').on('click', '.beginButton',
   function(event){
-    $('.startQuiz').remove();
+    $('.startQuiz').hide();
     $('main').append(renderQuestion);
+    updateQuestionNum();
   });
 };
 
 //Render Question after quiz start
 function renderQuestion(){
-  
+  $('.createForm').show();
+  if(questionCount < data.length){
+    return createQuestion(currentQuestion);
+  }else{
+    endOfQuiz();
+  }
 }
 
-function updateScoreQuestion(){
+function createQuestion(currentQuestion){
+  $('.createForm').html(
+    `<form role = "form" class = "formQuiz">
+        <h2>${data[currentQuestion].question}</h2>
+          <input name="option" type="radio" value = "${data[currentQuestion].answer[0]}" checked>
+          <label for="answer">${data[currentQuestion].answer[0]}</label><br>
+          <input name="option" type="radio" value = "${data[currentQuestion].answer[1]}">        
+          <label for="answer">${data[currentQuestion].answer[1]}</label><br>
+          <input name="option" type="radio" value = "${data[currentQuestion].answer[2]}">
+          <label for="option">${data[currentQuestion].answer[2]}</label><br>
+          <input name="option" type="radio" value = "${data[currentQuestion].answer[3]}">
+          <label for="answer">${data[currentQuestion].answer[3]}</label><br>
+            <button type="button" class="submitButton">Submit</button>
+      </form>`
+  )
+}
+
+function submitAnswer(){
+  $('.createForm').on('click', '.submitButton', function(event){
+    renderAppropriateFeedback();
+  });
+}
+
+function changeQuestionNum(){
+  if(questionCount < data.length){
+  questionCount++;
+  }
+}
+
+function updateQuestionNum(){
+  changeQuestionNum();
+  $('.questionNumber').text(questionCount);
+}
+
+function resetQuestionNum(){
+  questionCount = 0;
+  $('.questionNumber').text(questionCount);
+}
+
+function resetScore(){
+  score = 0;
+  $('.scoreNumber').text(score);
+}
+
+function changeScore(){
+  score++;
+}
+
+function updateScore(){
+  changeScore();
+  $('.scoreNumber').text(score);
+}
+
+function renderAppropriateFeedback(){
+  $('.createForm').hide();
+  $('.feedback').show();
+  let selected = $('input:checked');
+  let answer = selected.val();
+  console.log(answer);
+  let correctAns = data[currentQuestion].correct;
+  console.log(correctAns);
+    if (answer === correctAns) {
+      correctAnswer();
+    } else {
+      wrongAnswer();
+    }
 }
 
 function correctAnswer(){
+  $('.box').hide();
+  $('.feedback').html(
+    `<h3>Wow! You're Sharp!</h3>
+      <img src = 'images/knife.gif' alt = 'Dexter Morgan cleaning a knife'><br>
+      <button type = 'button' class = 'continueButton'>Continue...</button>`);
+  updateScore();
 }
 
 function wrongAnswer(){
+  $('.box').hide();
+  $('.feedback').html(
+    `<h3>Oops! Better luck next time!<br>The correct answer was "${data[currentQuestion].correct}"</h3>
+      <img src = 'images/oliviabenson.gif' alt = 'Olivia Benson shaking her head and walking away.'><br>
+      <button type = 'button' class = 'continueButton'>Continue...</button>`);
 }
+
+function nextQuestion(){
+  $('.feedback').on('click', '.continueButton',
+    function(event){
+      $('.feedback').hide();
+      $('.box').show();
+      currentQuestion++;
+      renderQuestion();
+      updateQuestionNum();
+    });
+  }
+
+function endOfQuiz(){
+  $('.box').hide();
+  $('.createForm').hide();
+  $('.feedback').hide();
+  $('.quizEnd').show();
+  $('.quizEnd').html(
+    `<h3>You Made It!<br><img src = 'images/batesmotel.gif' alt = 'Sherrif Alex Romero saying cheers and clinking glasses with Norma Bates.'><br>Your final score is ${score}/7</h3><button type = 'button' class = 'tryAgain'>Try Again?</button>`
+  )
+}
+
 
 function restartQuiz(){
+   $('.quizEnd').on('click', '.tryAgain', function(event){
+    resetQuestionNum();
+    resetScore();
+    $('.quizEnd').hide();
+    $('.startQuiz').show();
+  })
 }
-
 
 function handleQuizApp(){
   beginQuiz();
+  submitAnswer();
+  nextQuestion();
+  restartQuiz();
 }
 $(handleQuizApp);
 
--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 const STORE = [
   {
+    number: 1,
     question: 'Which murderer was famously known as the "Killer Clown"?',
     answer: [
       'Dennis Rader',
@@ -10,6 +11,7 @@ const STORE = [
     correct: 'John Wayne Gacy'
   },
   {
+    number: 2,
     question: 'What notorious female serial killer posed as a hitch-hiking sex worker to kill seven motorists?',
     answer: [
       'Nannie Doss',
@@ -20,6 +22,7 @@ const STORE = [
     correct: 'Aileen Wuornos'
   },
   {
+    number: 3,
     question: 'Which cannibal became known for attempting to turn their victims into zombies?',
     answer: [
       'Jeffrey Dahmer',
@@ -30,6 +33,7 @@ const STORE = [
     correct: 'Jeffrey Dahmer'
   },
   {
+    number: 4,
     question: 'Who was the child beauty queen who became victim to a still unsolved murder at the age of six in her families home?',
     answer: [
       'Tana Ramsay',
@@ -40,6 +44,7 @@ const STORE = [
     correct: 'JonBenét Ramsey'
   },
   {
+    number: 5,
     question: 'What name was given to kidnapper and suspected serial killer David Parker Ray; who is believed to have tortured and killed more than 50 women in his soundproof trailer?',
     answer: [
       'Mobile Home Murderer',
@@ -50,6 +55,7 @@ const STORE = [
     correct: 'Toy Box Killer'
   },
   {
+    number: 6,
     question: 'Who was the first serial killer to be caught using familial DNA after escaping capture for over 30 years?',
     answer: [
       'The Boston Strangler',
@@ -60,12 +66,13 @@ const STORE = [
     correct: 'The Golden State Killer'
   },
   {
+    number: 7,
     question: 'What did sixteen year old Brenda Ann Spencer respond when asked why she opened fire on an elementary school across from her home?',
     answer: [
-      'Why not?',
-      'I wanted to know what it felt like.',
+      "Why not?",
+      "I wanted to know what it felt like.",
       "I don't like Mondays.",
-      'I wanted to.'
+      "I wanted to."
     ],
     correct: "I don't like Mondays."
   }
